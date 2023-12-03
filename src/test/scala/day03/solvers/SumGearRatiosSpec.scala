0aed6dfcf2b61f15a5b977d3e389d874dd0be960
Moleem <imre.molnar.0225@gmail.com>
2023-12-03 13:47:53 +0100

    DAY-3 Well that was easy... worth wasting my whole sunday on

--- src/test/scala/day03/solvers/SumGearRatiosSpec.scala
+++ src/test/scala/day03/solvers/SumGearRatiosSpec.scala
@@ -1,12 +1,18 @@
 package day03.solvers
 
-import day03.model.{NumberRecord, AttachmentRecord}
+import day03.model.AttachmentRecord
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import utils.ProblemSolver
 
 object SumGearRatios extends ProblemSolver[Set[AttachmentRecord], Int] {
-  override def solve(input: Set[AttachmentRecord]): Int = ???
+  override def solve(input: Set[AttachmentRecord]): Int = {
+    input
+      .filter(_.shape == '*')
+      .filter(_.attachedNumbers.length == 2)
+      .map(_.attachedNumbers.product)
+      .sum
+  }
 }
 
 class SumGearRatiosSpec extends AnyFlatSpec with Matchers {
