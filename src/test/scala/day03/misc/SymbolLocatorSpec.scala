500d163677b790b5b336f5a39fe6b31e752742b5
Moleem <imre.molnar.0225@gmail.com>
2023-12-03 13:51:26 +0100

    DAY-3 Test fix and refactor

--- src/test/scala/day03/misc/SymbolLocatorSpec.scala
+++ src/test/scala/day03/misc/SymbolLocatorSpec.scala
@@ -1,6 +1,6 @@
 package day03.misc
 
-import day03.model.Point
+import day03.model.{Point, SymbolRecord}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
@@ -9,23 +9,39 @@ class SymbolLocatorSpec extends AnyFlatSpec with Matchers {
 
   behavior of "SymbolLocator"
 
+  it should "locate no symbols" in {
+    val input = ".5."
+    SymbolLocator.locateSymbols(input) shouldBe Set.empty
+  }
+
   it should "locate a single symbol in a single line" in {
     val input = "*"
-    SymbolLocator.locateSymbols(input) shouldBe Set(Point(0, 0))
+    SymbolLocator.locateSymbols(input) shouldBe Set(
+      SymbolRecord('*', Point(0, 0))
+    )
   }
 
   it should "locate a single nested symbol in a single line" in {
     val input = ".#5"
-    SymbolLocator.locateSymbols(input) shouldBe Set(Point(0, 1))
+    SymbolLocator.locateSymbols(input) shouldBe Set(
+      SymbolRecord('#', Point(0, 1))
+    )
   }
 
   it should "locate multiple nested symbols in a single line" in {
     val input = ".$5!."
-    SymbolLocator.locateSymbols(input) shouldBe Set(Point(0, 1), Point(0, 3))
+    SymbolLocator.locateSymbols(input) shouldBe Set(
+      SymbolRecord('$', Point(0, 1)),
+      SymbolRecord('!', Point(0, 3))
+    )
   }
 
   it should "locate multiple nested symbols in multiple lines" in {
     val input = ".*5*.\n....*"
-    SymbolLocator.locateSymbols(input) shouldBe Set(Point(0, 1), Point(0, 3), Point(1, 4))
+    SymbolLocator.locateSymbols(input) shouldBe Set(
+      SymbolRecord('*', Point(0, 1)),
+      SymbolRecord('*', Point(0, 3)),
+      SymbolRecord('*', Point(1, 4))
+    )
   }
 }
