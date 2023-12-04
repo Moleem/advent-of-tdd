90a9101697bf82f0836f65527b441b8c47f2afea
Moleem <imre.molnar.0225@gmail.com>
2023-12-04 08:12:26 +0100

    DAY-4 Task2 implementation

---
+++ src/main/scala/day04/solvers/CountCards.scala
@@ -0,0 +1,9 @@
+package day04.solvers
+
+import day04.model.ScratchCardRecord
+import utils.ProblemSolver
+
+object CountCards extends ProblemSolver[List[ScratchCardRecord], Int] {
+  override def solve(input: List[ScratchCardRecord]): Int =
+    ???
+}
