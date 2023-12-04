f8f3d092264ab82cde3d137d0b0a08793b19c201
Moleem <imre.molnar.0225@gmail.com>
2023-12-04 07:21:08 +0100

    DAY-4 Implementation added

--- src/test/scala/day04/solvers/SumWinScoresSpec.scala
+++ src/test/scala/day04/solvers/SumWinScoresSpec.scala
@@ -6,7 +6,13 @@ import org.scalatest.matchers.should.Matchers
 import utils.ProblemSolver
 
 object SumWinScores extends ProblemSolver[List[ScratchCardRecord], Int] {
-  override def solve(input: List[ScratchCardRecord]): Int = ???
+  override def solve(input: List[ScratchCardRecord]): Int =
+    input
+      .map(card => card.lotteryNumbers.intersect(card.ownNumbers))
+      .map(_.size)
+      .map(size => Math.pow(2, size - 1))
+      .map(_.toInt)
+      .sum
 }
 
 class SumWinScoresSpec extends AnyFlatSpec with Matchers {
