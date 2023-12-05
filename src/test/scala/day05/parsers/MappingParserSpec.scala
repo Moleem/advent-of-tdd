ba1fa6fe3f509285e8d9090821a3b3879432b931
Moleem <imre.molnar.0225@gmail.com>
2023-12-05 07:35:35 +0100

    DAY-5 test passing again

--- src/test/scala/day05/parsers/MappingParserSpec.scala
+++ src/test/scala/day05/parsers/MappingParserSpec.scala
@@ -52,45 +52,67 @@ class MappingParserSpec extends AnyFlatSpec with Matchers {
     val mappings = MappingParser.parse(mappingInput)
 
     mappings.seedToSoilMap should contain theSameElementsAs List(
-      MappingRange(from = 50, to = 97, delta = 2),
-      MappingRange(from = 98, to = 99, delta = -48),
+      MappingRange(from = 50, to = 97, delta = + 2),
+      MappingRange(from = 98, to = 99, delta = -48)
     )
   }
 
   it should "correctly parse soil to fertilizer map" in {
     val mappings = MappingParser.parse(mappingInput)
 
-    ???
+    mappings.soilToFertilizerMap should contain theSameElementsAs List(
+      MappingRange(from =  0, to = 14, delta = +39),
+      MappingRange(from = 15, to = 51, delta = -15),
+      MappingRange(from = 52, to = 53, delta = -15)
+    )
   }
 
   it should "correctly parse fertilizer to water map" in {
     val mappings = MappingParser.parse(mappingInput)
 
-    ???
+    mappings.fertilizerToWaterMap should contain theSameElementsAs List(
+      MappingRange(from =  0, to =  6, delta = +42),
+      MappingRange(from =  7, to = 10, delta = +50),
+      MappingRange(from = 11, to = 52, delta = -11),
+      MappingRange(from = 53, to = 60, delta = - 4)
+    )
   }
 
   it should "correctly parse water to light map" in {
     val mappings = MappingParser.parse(mappingInput)
 
-    ???
+    mappings.waterToLightMap should contain theSameElementsAs List(
+      MappingRange(from = 18, to = 24, delta = +70),
+      MappingRange(from = 25, to = 94, delta = - 7)
+    )
   }
 
   it should "correctly parse light to temperature map" in {
     val mappings = MappingParser.parse(mappingInput)
 
-    ???
+    mappings.lightToTemperatureMap should contain theSameElementsAs List(
+      MappingRange(from = 45, to = 63, delta = +36),
+      MappingRange(from = 64, to = 76, delta = + 4),
+      MappingRange(from = 77, to = 99, delta = -32)
+    )
   }
 
   it should "correctly parse temperature to humidity map" in {
     val mappings = MappingParser.parse(mappingInput)
 
-    ???
+    mappings.temperatureToHumidityMap should contain theSameElementsAs List(
+      MappingRange(from =  0, to = 68, delta = + 1),
+      MappingRange(from = 69, to = 69, delta = -69)
+    )
   }
 
   it should "correctly parse humidity to location map" in {
     val mappings = MappingParser.parse(mappingInput)
 
-    ???
+    mappings.humidityToLocationMap should contain theSameElementsAs List(
+      MappingRange(from = 56, to = 92, delta = + 4),
+      MappingRange(from = 93, to = 96, delta = -37)
+    )
   }
 
 }
