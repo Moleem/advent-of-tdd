8d98a95661d93c25205d0473fa99b0cac50e899b
Moleem <imre.molnar.0225@gmail.com>
2023-12-05 07:02:16 +0100

    DAY-5 refactor

--- src/test/scala/day05/solvers/FindLowestLocationSpec.scala
+++ src/test/scala/day05/solvers/FindLowestLocationSpec.scala
@@ -3,21 +3,7 @@ package day05.solvers
 import day05.model.Mappings
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
-import utils.ProblemSolver
 
-object FindLowestLocation extends ProblemSolver[Mappings, Int] {
-  override def solve(input: Mappings): Int =
-    input
-      .seeds
-      .map(input.seedToSoilMap)
-      .map(input.soilToFertilizerMap)
-      .map(input.fertilizerToWaterMap)
-      .map(input.waterToLightMap)
-      .map(input.lightToTemperatureMap)
-      .map(input.temperatureToHumidityMap)
-      .map(input.humidityToLocationMap)
-      .min
-}
 
 class FindLowestLocationSpec extends AnyFlatSpec with Matchers {
 
