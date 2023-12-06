78658a0e34ebbbb53573d1ecd0013a4b0064e48c
Moleem <imre.molnar.0225@gmail.com>
2023-12-06 06:58:36 +0100

    DAY-6 green

--- src/test/scala/day06/parsers/RaceDataParserSpec.scala
+++ src/test/scala/day06/parsers/RaceDataParserSpec.scala
@@ -9,7 +9,18 @@ case class RaceData(length: Long, recordDistance: Long) {
 }
 
 object RaceDataParser extends ContentParser[List[RaceData]] {
-  override def parse(content: String): List[RaceData] = ???
+  override def parse(content: String): List[RaceData] = {
+    val lines =
+      content.split("\n").toList
+
+    val timeLengths =
+      lines(0).split(":")(1).trim.split(" ").map(_.trim).filter(_.nonEmpty).map(_.toInt)
+
+    val recordDistances =
+      lines(1).split(":")(1).trim.split(" ").map(_.trim).filter(_.nonEmpty).map(_.toInt)
+
+    timeLengths.zip(recordDistances).toList.map { case (length, recordDistance) => RaceData(length, recordDistance)}
+  }
 }
 
 class RaceDataParserSpec extends AnyFlatSpec with Matchers {
