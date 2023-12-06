8483b6a61ba21f264b29a78571bb14b6566b6753
Moleem <imre.molnar.0225@gmail.com>
2023-12-06 07:30:41 +0100

    DAY-6 refactor

--- src/main/scala/day06/model/RaceData.scala
+++ src/main/scala/day06/model/RaceData.scala
@@ -1,3 +1,3 @@
 package day06.model
 
-case class RaceData(length: Int, recordDistance: Int)
+case class RaceData(length: Long, recordDistance: Long)
--- src/main/scala/day06/solvers/SumRecordBreakingOptions.scala
+++ src/main/scala/day06/solvers/SumRecordBreakingOptions.scala
@@ -5,10 +5,21 @@ import utils.ProblemSolver
 
 object SumRecordBreakingOptions extends ProblemSolver[List[RaceData], Long] {
   override def solve(input: List[RaceData]): Long = {
-    input.map { race =>
-      (0 to race.length).count(pressTime =>
-        pressTime * (race.length - pressTime) > race.recordDistance
-      )
-    }.product
+    var product = 1L
+
+    input.foreach { race =>
+      var waysToWin = 0
+      var i = 0
+        while (i<race.length) {
+          if (i * (race.length - i) > race.recordDistance) {
+            waysToWin += 1
+          }
+          i+=1
+        }
+
+      product = product*waysToWin
+    }
+
+    product
   }
 }
