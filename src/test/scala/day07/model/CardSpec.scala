a766b4e5dd14f1c1dbaeccea163e37739bff8baf
Moleem <imre.molnar.0225@gmail.com>
2023-12-07 07:28:47 +0100

    DAY-7 red

--- src/test/scala/day07/model/CardSpec.scala
+++ src/test/scala/day07/model/CardSpec.scala
@@ -9,8 +9,7 @@ class CardSpec extends AnyFlatSpec with Matchers {
   it should "have a defined order" in {
     C_A compare C_K shouldBe 1
     C_K compare C_Q shouldBe 1
-    C_Q compare C_J shouldBe 1
-    C_J compare C_T shouldBe 1
+    C_Q compare C_T shouldBe 1
     C_T compare C_9 shouldBe 1
     C_9 compare C_8 shouldBe 1
     C_8 compare C_7 shouldBe 1
@@ -19,5 +18,6 @@ class CardSpec extends AnyFlatSpec with Matchers {
     C_5 compare C_4 shouldBe 1
     C_4 compare C_3 shouldBe 1
     C_3 compare C_2 shouldBe 1
+    C_2 compare C_J shouldBe 1
   }
 }
