964289da7c8328b6f6a65617b9b12e3a88b0598a
Moleem <imre.molnar.0225@gmail.com>
2023-12-07 07:25:57 +0100

    DAY-7 task 1 solved

---
+++ src/main/scala/day07/Runner.scala
@@ -0,0 +1,23 @@
+package day07
+
+import day07.parsers.HandBidParser
+import day07.solvers.TotalWinningCalculator
+import utils.PrintSolution
+
+object Runner extends App {
+
+  val dayNum = this.getClass.getPackageName
+
+  PrintSolution(
+    inputFileName = s"/$dayNum/input-1.txt",
+    contentParser = HandBidParser,
+    problemSolver = TotalWinningCalculator
+  )
+
+//  PrintSolution(
+//    inputFileName = s"/$dayNum/input-2.txt",
+//    contentParser = DummyTask2Parser,
+//    problemSolver = DummyTask2Solver
+//  )
+
+}
