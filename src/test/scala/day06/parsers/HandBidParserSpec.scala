361a54b2bbbbd5e24c322aaca66a5750a4340487
Moleem <imre.molnar.0225@gmail.com>
2023-12-07 07:03:18 +0100

    DAY-7 TotalWinningCalculator test

--- src/test/scala/day06/parsers/HandBidParserSpec.scala
+++ src/test/scala/day06/parsers/HandBidParserSpec.scala
@@ -2,7 +2,7 @@ package day06.parsers
 
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
-import utils.ContentParser
+import utils.{ContentParser, ProblemSolver}
 
 sealed trait Card {
   val strength: Int
@@ -82,6 +82,10 @@ object HandBidParser extends ContentParser[List[HandBid]] {
       }
 }
 
+object TotalWinningCalculator extends ProblemSolver[List[HandBid], Int] {
+  override def solve(input: List[HandBid]): Int = ???
+}
+
 class CardSpec extends AnyFlatSpec with Matchers {
   behavior of "Card"
 
@@ -138,5 +142,28 @@ class HandBidParserSpec extends AnyFlatSpec with Matchers {
       HandBid(Hand("KTJJT"), 220),
       HandBid(Hand("QQQJA"), 483)
     )
+  }
+}
+
+class TotalWinningCalculatorSpec extends AnyFlatSpec with Matchers {
+
+  behavior of "TotalWinningCalculator"
+
+  it should "multiply rank with bid" in {
+    val input = List(HandBid(Hand("AAA22"), 25)) // only one hand, so rank 1
+
+    TotalWinningCalculator.solve(input) shouldBe 25
+  }
+
+  it should "sum rank-bid products" in {
+    val input = List(
+      HandBid(Hand("32T3K"), 765), // rank 1
+      HandBid(Hand("T55J5"), 684), // rank 4
+      HandBid(Hand("KK677"), 28),  // rank 3
+      HandBid(Hand("KTJJT"), 220), // rank 2
+      HandBid(Hand("QQQJA"), 483)  // rank 5
+    )
+
+    TotalWinningCalculator.solve(input) shouldBe 6440 // 765 * 1 + 220 * 2 + 28 * 3 + 684 * 4 + 483 * 5
   }
 }
