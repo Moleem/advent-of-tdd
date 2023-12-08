bb27fedaede2001e646e65d10028647b10e2afb2
Moleem <imre.molnar.0225@gmail.com>
2023-12-08 12:04:58 +0100

    DAY-8 red

---
+++ src/test/scala/day08/solvers/CountStepsSimultaneouslySpec.scala
@@ -0,0 +1,48 @@
+package day08.solvers
+
+import day08.model.MovementMap
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import utils.ProblemSolver
+
+object CountStepsSimultaneously extends ProblemSolver[MovementMap, Int] {
+  override def solve(input: MovementMap): Int = ???
+}
+
+class CountStepsSimultaneouslySpec extends AnyFlatSpec with Matchers {
+
+  behavior of CountStepsSimultaneously.getClass.getName
+
+  it should "find step count if the target is immediate (left)" in {
+    val input = MovementMap(
+      directions = List('L'), mappings = Map(
+        "11A" -> ("11Z", "___"),
+        "22A" -> ("22Z", "___")
+      )
+    )
+
+    CountStepsSimultaneously.solve(input) shouldBe 1
+  }
+
+  //  it should "find step count if the target is immediate (right)" in {
+//    val input = MovementMap(
+//      directions = List('R'), mappings = Map(
+//        "AAA" -> ("___", "ZZZ")
+//      )
+//    )
+//
+//    CountSteps.solve(input) shouldBe 1
+//  }
+//
+//  it should "find step count if the target is within one loop" in {
+//    val input = MovementMap(
+//      directions = List('L', 'L'), mappings = Map(
+//        "AAA" -> ("BBB", "___"),
+//        "BBB" -> ("ZZZ", "___")
+//      )
+//    )
+//
+//    CountSteps.solve(input) shouldBe 2
+//  }
+
+}
