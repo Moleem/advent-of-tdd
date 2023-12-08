2340fb5545ff7a7765d2910e52541885ad98ea3d
Moleem <imre.molnar.0225@gmail.com>
2023-12-08 21:11:01 +0100

    DAY-8 refactor

--- src/main/scala/day08/solvers/CountStepsSimultaneously.scala
+++ src/main/scala/day08/solvers/CountStepsSimultaneously.scala
@@ -5,32 +5,51 @@ import utils.ProblemSolver
 
 import scala.annotation.tailrec
 
-object CountStepsSimultaneously extends ProblemSolver[MovementMap, Int] {
-  override def solve(input: MovementMap): Int =
-    countSteps(input, input.mappings.keySet.filter(_.endsWith("A")))
+object CountStepsSimultaneously extends ProblemSolver[MovementMap, Long] {
 
   @tailrec
-  private def countSteps(
-                          input: MovementMap,
-                          keys: Set[String],
-                          stepCount: Int = 0,
-                          seenInputs: Set[Set[String]] = Set.empty
-                        ): Int = {
-    if (seenInputs.contains(keys)) throw new RuntimeException
-    else if (keys.forall(_.endsWith("Z"))) stepCount
-    else {
-      if (stepCount % 1000 == 0) {
-        println(stepCount)
-        println(keys)
+  private def getNextZValueAndDistance(input: MovementMap, key: String, stepCount: Int = 1): (String, Int) = {
+    val directionIndex = (stepCount-1) % input.directions.size
+    val direction = input.directions(directionIndex)
+    val nextKey =
+      direction match {
+        case 'L' => input.mappings(key)._1
+        case 'R' => input.mappings(key)._2
       }
-      val directionIndex = stepCount % input.directions.size
-      val direction = input.directions(directionIndex)
-      val nextKeys = keys.map(key =>
-        direction match {
-          case 'L' => input.mappings(key)._1
-          case 'R' => input.mappings(key)._2
-        })
-      countSteps(input, nextKeys, stepCount + 1, (keys :: seenInputs.toList).toSet)
+
+    if (nextKey.endsWith("Z"))
+      (nextKey, stepCount)
+    else {
+      getNextZValueAndDistance(input, nextKey, stepCount + 1)
     }
   }
+
+
+  override def solve(input: MovementMap): Long = {
+    val distanceMetrics = input
+      .mappings
+      .keySet
+      .filter(_.endsWith("A"))
+      .map{ key =>
+        val (nextX, initialDistanceToX) = getNextZValueAndDistance(input, key)
+        val (lastX, repeatingLength) = getNextZValueAndDistance(input, nextX)
+        (initialDistanceToX, repeatingLength)
+      }.toList
+
+    val startDistance = distanceMetrics.head._1
+    val repeatDistance = distanceMetrics.head._2
+
+    @tailrec
+    def helper(current: Long): Long = {
+      if (distanceMetrics.forall(m => (current-m._1)%m._2 == 0))
+        current
+      else
+        helper(current + repeatDistance)
+    }
+
+
+    helper(startDistance)
+
+  }
+
 }
--- src/test/scala/day08/solvers/CountStepsSimultaneouslySpec.scala
+++ src/test/scala/day08/solvers/CountStepsSimultaneouslySpec.scala
@@ -13,7 +13,9 @@ class CountStepsSimultaneouslySpec extends AnyFlatSpec with Matchers {
     val input = MovementMap(
       directions = List('L'), mappings = Map(
         "11A" -> ("11Z", "___"),
-        "22A" -> ("22Z", "___")
+        "11Z" -> ("11Z", "___"),
+        "22A" -> ("22Z", "___"),
+        "22Z" -> ("22Z", "___")
       )
     )
 
@@ -24,7 +26,9 @@ class CountStepsSimultaneouslySpec extends AnyFlatSpec with Matchers {
     val input = MovementMap(
       directions = List('R'), mappings = Map(
         "11A" -> ("___", "11Z"),
-        "22A" -> ("___", "22Z")
+        "11Z" -> ("___", "11Z"),
+        "22A" -> ("___", "22Z"),
+        "22Z" -> ("___", "22Z")
       )
     )
 
@@ -33,15 +37,112 @@ class CountStepsSimultaneouslySpec extends AnyFlatSpec with Matchers {
 
   it should "find step count if the target is within one loop" in {
     val input = MovementMap(
-      directions = List('L', 'L'), mappings = Map(
+      directions = List('L', 'R'), mappings = Map(
         "11A" -> ("11B", "___"),
+        "11B" -> ("___", "11Z"),
+        "11Z" -> ("11Z", "11Z"),
         "22A" -> ("22B", "___"),
-        "11B" -> ("11Z", "___"),
-        "22B" -> ("22Z", "___"),
+        "22B" -> ("___", "22Z"),
+        "22Z" -> ("22Z", "22Z"),
       )
     )
 
     CountStepsSimultaneously.solve(input) shouldBe 2
   }
 
+
+  it should "find step count if the target is beyond one loop" in {
+    val input = MovementMap(
+      directions = List('L', 'R'), mappings = Map(
+        "11A" -> ("11B", "___"),
+        "11B" -> ("___", "11C"),
+        "11C" -> ("11Z", "___"),
+        "11Z" -> ("11Z", "11Z"),
+        "22A" -> ("22B", "___"),
+        "22B" -> ("___", "22C"),
+        "22C" -> ("22Z", "___"),
+        "22Z" -> ("22Z", "22Z")
+      )
+    )
+
+    CountStepsSimultaneously.solve(input) shouldBe 3
+  }
+
+  it should "find step count for targets with different repeat cycles (quicker)" in {
+    val input: MovementMap = MovementMap(
+      directions = List('L'),
+      mappings = Map(
+        "11A" -> ("11Z", "___"),
+        "11Z" -> ("11Z", "___"),
+        "22A" -> ("22B", "___"),
+        "22B" -> ("22C", "___"),
+        "22C" -> ("22Z", "___"),
+        "22Z" -> ("22Z", "___"),
+      )
+    )
+
+    CountStepsSimultaneously.solve(input) shouldBe 3
+  }
+
+  it should "find step count for targets with different repeat cycles (longer)" in {
+    val input: MovementMap = MovementMap(
+      directions = List('L'),
+      mappings = Map(
+        "11A" -> ("11B", "___"),
+        "11B" -> ("11C", "___"),
+        "11C" -> ("11D", "___"),
+        "11D" -> ("11E", "___"),
+        "11E" -> ("11Z", "___"),
+        "11Z" -> ("11F", "___"), // <- first Z and cycle start
+        "11F" -> ("11G", "___"),
+        "11G" -> ("11H", "___"),
+        "11H" -> ("11I", "___"),
+        "11I" -> ("11Z", "___"), // <- second Z and cycle end
+        "22A" -> ("22B", "___"),
+        "22B" -> ("22C", "___"),
+        "22C" -> ("22D", "___"),
+        "22D" -> ("22E", "___"),
+        "22E" -> ("22F", "___"),
+        "22F" -> ("22G", "___"),
+        "22G" -> ("22Z", "___"), // <- first Z and cycle start
+        "22Z" -> ("22H", "___"),
+        "22H" -> ("22I", "___"),
+        "22I" -> ("22J", "___"),
+        "22J" -> ("22K", "___"),
+        "22K" -> ("22L", "___"),
+        "22L" -> ("22M", "___"),
+        "22M" -> ("22Z", "___"),
+      )
+    )
+
+    CountStepsSimultaneously.solve(input) shouldBe 35
+  }
+
+  it should "solve the official example" in {
+    // LR
+    //
+    //11A = (11B, XXX)
+    //11B = (XXX, 11Z)
+    //11Z = (11B, XXX)
+    //22A = (22B, XXX)
+    //22B = (22C, 22C)
+    //22C = (22Z, 22Z)
+    //22Z = (22B, 22B)
+    //XXX = (XXX, XXX)
+    val input: MovementMap = MovementMap(
+      directions = List('L', 'R'),
+      mappings = Map(
+        "11A" -> ("11B", "XXX"),
+        "11B" -> ("XXX", "11Z"),
+        "11Z" -> ("11B", "XXX"),
+        "22A" -> ("22B", "XXX"),
+        "22B" -> ("22C", "22C"),
+        "22C" -> ("22Z", "22Z"),
+        "22Z" -> ("22B", "22B"),
+        "XXX" -> ("XXX", "XXX")
+      )
+    )
+
+    CountStepsSimultaneously.solve(input) shouldBe 6
+  }
 }
