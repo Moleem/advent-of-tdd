2b63d6c7f697a7bc09cae7f510249f32cb65f552
Moleem <imre.molnar.0225@gmail.com>
2023-12-08 12:12:41 +0100

    DAY-8 green

--- src/test/scala/day08/solvers/CountStepsSimultaneouslySpec.scala
+++ src/test/scala/day08/solvers/CountStepsSimultaneouslySpec.scala
@@ -5,17 +5,43 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import utils.ProblemSolver
 
-object CountStepsSimultaneously extends ProblemSolver[MovementMap, Int] {
-  override def solve(input: MovementMap): Int = {
-    val keysEndingWithA = input.mappings.keys.filter(_.endsWith("A"))
+import scala.annotation.tailrec
 
-    input.directions.head match {
-      case 'L' =>
-        if (keysEndingWithA.forall(key => input.mappings(key)._1.endsWith("Z"))) 1 else ???
-      case 'R' =>
-        if (keysEndingWithA.forall(key => input.mappings(key)._2.endsWith("Z"))) 1 else ???
+object CountStepsSimultaneously extends ProblemSolver[MovementMap, Int] {
+  override def solve(input: MovementMap): Int =
+    countSteps(input, input.mappings.keySet.filter(_.endsWith("A")))
+
+
+  @tailrec
+  private def countSteps(
+                          input: MovementMap,
+                          keys: Set[String],
+                          stepCount: Int = 0
+                        ): Int = {
+    if (keys.forall(_.endsWith("Z"))) stepCount
+    else {
+      val directionIndex = stepCount % input.directions.size
+      val direction = input.directions(directionIndex)
+      val nextKeys = keys.map(key =>
+        direction match {
+          case 'L' => input.mappings(key)._1
+          case 'R' => input.mappings(key)._2
+        })
+      countSteps(input, nextKeys, stepCount + 1)
     }
   }
+
+
+  //  {
+  //    val keysEndingWithA = input.mappings.keys.filter(_.endsWith("A"))
+  //
+  //    input.directions.head match {
+  //      case 'L' =>
+  //        if (keysEndingWithA.forall(key => input.mappings(key)._1.endsWith("Z"))) 1 else ???
+  //      case 'R' =>
+  //        if (keysEndingWithA.forall(key => input.mappings(key)._2.endsWith("Z"))) 1 else ???
+  //    }
+  //  }
 }
 
 class CountStepsSimultaneouslySpec extends AnyFlatSpec with Matchers {
@@ -33,18 +59,18 @@ class CountStepsSimultaneouslySpec extends AnyFlatSpec with Matchers {
     CountStepsSimultaneously.solve(input) shouldBe 1
   }
 
-    it should "find step count if the target is immediate (right)" in {
-      val input = MovementMap(
-        directions = List('R'), mappings = Map(
-          "11A" -> ("___", "11Z"),
-          "22A" -> ("___", "22Z")
-        )
+  it should "find step count if the target is immediate (right)" in {
+    val input = MovementMap(
+      directions = List('R'), mappings = Map(
+        "11A" -> ("___", "11Z"),
+        "22A" -> ("___", "22Z")
       )
+    )
 
-      CountStepsSimultaneously.solve(input) shouldBe 1
+    CountStepsSimultaneously.solve(input) shouldBe 1
   }
 
-    it should "find step count if the target is within one loop" in {
+  it should "find step count if the target is within one loop" in {
     val input = MovementMap(
       directions = List('L', 'L'), mappings = Map(
         "11A" -> ("11B", "___"),
@@ -54,7 +80,7 @@ class CountStepsSimultaneouslySpec extends AnyFlatSpec with Matchers {
       )
     )
 
-    CountSteps.solve(input) shouldBe 2
+    CountStepsSimultaneously.solve(input) shouldBe 2
   }
 
 }
