700960cc159f66f788c216ff45d1723ee62f5ab7
Moleem <imre.molnar.0225@gmail.com>
2023-12-09 09:45:47 +0100

    DAY-9 (little detour) green

--- src/main/scala/utils/ProblemSolver.scala
+++ src/main/scala/utils/ProblemSolver.scala
@@ -2,5 +2,9 @@ package utils
 
 trait ProblemSolver[T, R] {
   def solve(input: T): R
-  def andThen[U](other: ProblemSolver[R, U]): ProblemSolver[T, U] = ???
+
+  def andThen[U](other: ProblemSolver[R, U]): ProblemSolver[T, U] = {
+    (input: T) => other.solve(this.solve(input))
+  }
+
 }
