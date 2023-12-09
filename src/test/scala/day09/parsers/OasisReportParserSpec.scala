21bd5e62a3b66557d5cf5870d9a26fa8e562e804
Moleem <imre.molnar.0225@gmail.com>
2023-12-09 09:31:08 +0100

    DAY-9 red

--- src/test/scala/day09/parsers/OasisReportParserSpec.scala
+++ src/test/scala/day09/parsers/OasisReportParserSpec.scala
@@ -7,4 +7,18 @@ class OasisReportParserSpec extends AnyFlatSpec with Matchers {
 
   behavior of OasisReportParser.getClass.getName
 
+  it should "parse number sequences" in {
+    val input =
+      """0 3 6 9 12 15
+        |1 3 6 10 15 21
+        |10 13 16 21 30 45""".stripMargin
+
+    val expectedOutput = List(
+      List(0, 3, 6, 9, 12, 15),
+      List(1, 3, 6, 10, 15, 21),
+      List(10, 13, 16, 21, 30, 45),
+    )
+
+    OasisReportParser.parse(input) shouldBe expectedOutput
+  }
 }
