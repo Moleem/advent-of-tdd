0a0df82791348888d7f0bcdcad5aed6f6db28b4e
Moleem <imre.molnar.0225@gmail.com>
2023-12-09 09:32:05 +0100

    DAY-9 green

--- src/main/scala/day09/parsers/OasisReportParser.scala
+++ src/main/scala/day09/parsers/OasisReportParser.scala
@@ -3,5 +3,11 @@ package day09.parsers
 import utils.ContentParser
 
 object OasisReportParser extends ContentParser[List[List[Int]]] {
-  override def parse(content: String): List[List[Int]] = ???
+
+  override def parse(content: String): List[List[Int]] =
+    content
+      .split("\n")
+      .toList
+      .map(_.split(" ").toList.map(_.toInt))
+
 }
