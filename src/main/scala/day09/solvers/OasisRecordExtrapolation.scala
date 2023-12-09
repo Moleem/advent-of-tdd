adf3b80694337dc8c1e508742673d96ce59ec878
Moleem <imre.molnar.0225@gmail.com>
2023-12-09 10:03:28 +0100

    DAY-9 green

--- src/main/scala/day09/solvers/OasisRecordExtrapolation.scala
+++ src/main/scala/day09/solvers/OasisRecordExtrapolation.scala
@@ -5,7 +5,7 @@ import utils.ProblemSolver
 object OasisRecordExtrapolation extends ProblemSolver[List[List[Int]], List[Int]] {
 
   override def solve(input: List[List[Int]]): List[Int] =
-    List(extrapolate(input.head))
+    input.map(extrapolate)
 
 
   private def extrapolate(input: List[Int]): Int =
