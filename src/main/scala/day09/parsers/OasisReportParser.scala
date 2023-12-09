ce45fd45f23283d6f8ab6d5398302db46ab7b99e
Moleem <imre.molnar.0225@gmail.com>
2023-12-09 09:29:14 +0100

    DAY-9 start

---
+++ src/main/scala/day09/parsers/OasisReportParser.scala
@@ -0,0 +1,7 @@
+package day09.parsers
+
+import utils.ContentParser
+
+object OasisReportParser extends ContentParser[List[List[Int]]] {
+  override def parse(content: String): List[List[Int]] = ???
+}
---
+++ src/test/scala/day09/parsers/OasisReportParserSpec.scala
@@ -0,0 +1,10 @@
+package day09.parsers
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class OasisReportParserSpec extends AnyFlatSpec with Matchers {
+
+  behavior of OasisReportParser.getClass.getName
+
+}
