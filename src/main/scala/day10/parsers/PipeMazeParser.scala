83993d8b89452c9ba40e2352313d10a1e392e1f5
Moleem <imre.molnar.0225@gmail.com>
2023-12-10 08:24:20 +0100

    DAY-10 green

--- src/main/scala/day10/parsers/PipeMazeParser.scala
+++ src/main/scala/day10/parsers/PipeMazeParser.scala
@@ -3,5 +3,9 @@ package day10.parsers
 import utils.ContentParser
 
 object PipeMazeParser extends ContentParser[List[List[Char]]] {
-  override def parse(content: String): List[List[Char]] = ???
+  override def parse(content: String): List[List[Char]] =
+    content
+      .split("\n")
+      .toList
+      .map(_.toList)
 }
