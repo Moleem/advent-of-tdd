9ecfae5acbf3213697abccbe7d89705ef4405091
Moleem <imre.molnar.0225@gmail.com>
2023-12-10 10:13:01 +0100

    DAY-10 red

--- src/test/scala/day10/solvers/CountStepsSpec.scala
+++ src/test/scala/day10/solvers/CountStepsSpec.scala
@@ -7,11 +7,21 @@ class CountStepsSpec extends AnyFlatSpec with Matchers {
 
   behavior of CountSteps.getClass.getName
 
-  it should "be able to count steps down" in {
+  it should "be able to count steps south" in {
     val input = List(
       List('.', 'S', '.'),
       List('.', '|', '.'),
       List('.', 'S', '.')
+    )
+
+    CountSteps.solve(input) shouldBe 2
+  }
+
+  it should "be able to count steps east" in {
+    val input = List(
+      List('.', '.', '.'),
+      List('S', '-', 'S'),
+      List('.', '.', '.')
     )
 
     CountSteps.solve(input) shouldBe 2
