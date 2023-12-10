3b39cd5dbd9563636b67d7f3451226e1da570a60
Moleem <imre.molnar.0225@gmail.com>
2023-12-10 14:51:34 +0100

    DAY-10 red

--- src/test/scala/day10/solvers/MarkEnclosureSpec.scala
+++ src/test/scala/day10/solvers/MarkEnclosureSpec.scala
@@ -7,7 +7,7 @@ class MarkEnclosureSpec extends AnyFlatSpec with Matchers {
 
   behavior of MarkEnclosure.getClass.getName
 
-  it should "create an outer border around the original image" in {
+  it should "create an outer border around the original input" in {
     val input = List(
       List('.')
     )
@@ -15,6 +15,21 @@ class MarkEnclosureSpec extends AnyFlatSpec with Matchers {
       List('.', '.', '.'),
       List('.', '.', '.'),
       List('.', '.', '.')
+    )
+
+    MarkEnclosure.solve(input) shouldBe expectedOutput
+  }
+
+  it should "keep the original pipes intact" in {
+    val input = List(
+      List('S','7'),
+      List('L','J')
+    )
+    val expectedOutput = List(
+      List('.', '.', '.', '.'),
+      List('.', 'S', '7', '.'),
+      List('.', 'L', 'J', '.'),
+      List('.', '.', '.', '.')
     )
 
     MarkEnclosure.solve(input) shouldBe expectedOutput
