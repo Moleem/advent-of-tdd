18e3a2e695ee3436475cd508d4e8bac4384d733c
Moleem <imre.molnar.0225@gmail.com>
2023-12-10 10:26:27 +0100

    DAY-10 green

--- src/main/scala/day10/solvers/CountSteps.scala
+++ src/main/scala/day10/solvers/CountSteps.scala
@@ -27,21 +27,26 @@ object CountSteps extends ProblemSolver[List[List[Char]], Int] {
         case ('S', direction) if direction.isEmpty =>
           val (southRow, southCol) = getSouthCoordinates(currentRow, currentCol)
           val (eastRow, eastCol) = getEastCoordinates(currentRow, currentCol)
+          val (westRow, westCol) = getWestCoordinates(currentRow, currentCol)
 
           if (canMoveToSouth(southRow, southCol))
             countSteps("north", southRow, southCol, currentStepCount+1)
           else if (canMoveToEast(eastRow, eastCol))
-            countSteps("west", eastRow, eastCol, currentStepCount+1)
+            countSteps("west", eastRow, eastCol, currentStepCount + 1)
           else ???
         case ('|', "north") =>
           val (southRow, southCol) = getSouthCoordinates(currentRow, currentCol)
           countSteps("north", southRow, southCol, currentStepCount + 1)
         case ('L', "north") =>
           val (eastRow, eastCol) = getEastCoordinates(currentRow, currentCol)
-          countSteps("west", eastRow, eastCol, currentStepCount+1)
+          countSteps("west", eastRow, eastCol, currentStepCount + 1)
+        case ('J', "north") =>
+          val (westRow, westCol) = getWestCoordinates(currentRow, currentCol)
+          countSteps("east", westRow, westCol, currentStepCount + 1)
         case ('-', "west") =>
           val (eastRow, eastCol) = getEastCoordinates(currentRow, currentCol)
-          countSteps("west", eastRow, eastCol, currentStepCount+1)
+          countSteps("west", eastRow, eastCol, currentStepCount + 1)
+        case _ => ???
       }
 
     def getSouthCoordinates(currentRow: Int, currentCol: Int): (Int, Int) =
@@ -50,15 +55,25 @@ object CountSteps extends ProblemSolver[List[List[Char]], Int] {
     def getEastCoordinates(currentRow: Int, currentCol: Int): (Int, Int) =
       (currentRow, currentCol + 1)
 
+    def getWestCoordinates(currentRow: Int, currentCol: Int): (Int, Int) =
+      (currentRow, currentCol - 1)
+
     def canMoveToSouth(row: Int, col: Int): Boolean =
       0 <= row && row < input.size &&
-        0 <= col && col < input.head.size &&
-        Set('|', 'J', 'L').contains(input(row)(col))
+      0 <= col && col < input.head.size &&
+      Set('|', 'J', 'L').contains(input(row)(col))
 
     def canMoveToEast(row: Int, col: Int): Boolean =
       0 <= row && row < input.size &&
-        0 <= col && col < input.head.size &&
-        Set('-', 'J', '7').contains(input(row)(col))
+      0 <= col && col < input.head.size &&
+      Set('-', 'J', '7').contains(input(row)(col))
+
+    def canMoveToWest(row: Int, col: Int): Boolean =
+      0 <= row && row < input.size &&
+      0 <= col && col < input.head.size &&
+      Set('-', 'F', 'L').contains(input(row)(col))
+
+
 
     val (startRow, startCol) = findStart(0, 0)
     countSteps("", startRow, startCol, 0)
