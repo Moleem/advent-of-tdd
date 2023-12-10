13486839bfa206f41aa80482ca1e8de3d9254719
Moleem <imre.molnar.0225@gmail.com>
2023-12-10 08:23:45 +0100

    DAY-10 red

---
+++ src/main/scala/day10/parsers/PipeMazeParser.scala
@@ -0,0 +1,7 @@
+package day10.parsers
+
+import utils.ContentParser
+
+object PipeMazeParser extends ContentParser[List[List[Char]]] {
+  override def parse(content: String): List[List[Char]] = ???
+}
