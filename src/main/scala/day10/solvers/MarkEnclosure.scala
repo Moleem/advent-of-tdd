3b649c06b5fad6f8bbc2567cbaecb6fb16fc09a9
Moleem <imre.molnar.0225@gmail.com>
2023-12-10 14:49:37 +0100

    DAY-10 green

--- src/main/scala/day10/solvers/MarkEnclosure.scala
+++ src/main/scala/day10/solvers/MarkEnclosure.scala
@@ -5,6 +5,15 @@ import utils.ProblemSolver
 import scala.annotation.tailrec
 
 object MarkEnclosure extends ProblemSolver[List[List[Char]], List[List[Char]]] {
-  override def solve(input: List[List[Char]]): List[List[Char]] = ???
+  override def solve(input: List[List[Char]]): List[List[Char]] = {
+    val rowCount = input.size + 2
+    val colCount = input.head.size + 2
+
+    (0 until rowCount).map { row =>
+      (0 until colCount).map { col =>
+        '.'
+      }.toList
+    }.toList
+  }
 
 }
