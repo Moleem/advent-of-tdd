2a997ad1d02b1eef496d28226e3a5d2314760fc4
Moleem <imre.molnar.0225@gmail.com>
2023-12-11 16:54:51 +0100

    DAY-11 green

--- src/main/scala/day11/parsers/GalaxyMapParser.scala
+++ src/main/scala/day11/parsers/GalaxyMapParser.scala
@@ -2,20 +2,28 @@ package day11.parsers
 
 import utils.ContentParser
 
-import scala.collection.mutable
-
 object GalaxyMapParser extends ContentParser[ Map[Int, (Int, Int)]] {
   override def parse(content: String):  Map[Int, (Int, Int)] = {
     val matrix = content.split("\n").toList.map(_.toList)
 
-    matrix.indices.flatMap { row =>
+    def getNumberOfEmptyRowsBefore(row: Int): Int =
+      (0 until row).count(id => matrix(id).forall(_ == '.'))
+
+
+    val galaxies = matrix.indices.flatMap { row =>
       matrix.head.indices.map { col =>
         (row, col)
       }
     }
       .filter { case (row, col) => matrix(row)(col) == '#'}
       .zipWithIndex
-      .map {case ((row, col), id) => id+1 -> (row, col) }
+      .map {case ((row, col), id) => id -> (row, col) }
       .toMap
+
+    galaxies.map { case (id, (row, col)) =>
+      val numberOfEmptyRowsBefore = getNumberOfEmptyRowsBefore(row)
+      id -> (row + numberOfEmptyRowsBefore, col)
+    }
   }
+
 }
