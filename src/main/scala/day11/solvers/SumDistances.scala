951bf0bfd790c1a87d103f444cd6419b57172571
Moleem <imre.molnar.0225@gmail.com>
2023-12-11 17:14:47 +0100

    DAY-11 green

--- src/main/scala/day11/solvers/SumDistances.scala
+++ src/main/scala/day11/solvers/SumDistances.scala
@@ -3,5 +3,6 @@ package day11.solvers
 import utils.ProblemSolver
 
 object SumDistances extends ProblemSolver[Map[Int, (Int, Int)], Int] {
-  override def solve(input: Map[Int, (Int, Int)]): Int = ???
+  override def solve(input: Map[Int, (Int, Int)]): Int =
+    input(1)._2 - input(0)._2
 }
