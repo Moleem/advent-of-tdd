ffb5398c2210f67993eb1d0c797f7ea62a62932b
Moleem <imre.molnar.0225@gmail.com>
2023-12-12 11:17:35 +0100

    DAY-12 green

--- src/main/scala/day12/parsers/SpringMapParser.scala
+++ src/main/scala/day12/parsers/SpringMapParser.scala
@@ -3,5 +3,15 @@ package day12.parsers
 import utils.ContentParser
 
 object SpringMapParser extends ContentParser[List[(String, List[Int])]] {
-  override def parse(content: String): List[(String, List[Int])] = ???
+  override def parse(content: String): List[(String, List[Int])] =
+    content
+      .split("\n")
+      .toList
+      .map(line => {
+        val parts = line.split(" ")
+        val chars = parts(0)
+        val nums = parts(1).split(",").map(_.toInt).toList
+
+        (chars, nums)
+      })
 }
