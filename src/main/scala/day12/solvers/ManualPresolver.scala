0b89cc787a6eb785f26d787811878980f64bdc2c
Moleem <imre.molnar.0225@gmail.com>
2023-12-12 22:18:50 +0100

    DAY-12 red

--- src/main/scala/day12/solvers/ManualPresolver.scala
+++ src/main/scala/day12/solvers/ManualPresolver.scala
@@ -5,5 +5,14 @@ import utils.ProblemSolver
 import scala.collection.mutable
 
 object ManualPresolver extends ProblemSolver[List[(String, List[Int])], List[(String, List[Int])]] {
-  override def solve(input: List[(String, List[Int])]): List[(String, List[Int])] = ???
+  override def solve(input: List[(String, List[Int])]): List[(String, List[Int])] =
+    input.map { case (pattern, nums) =>
+      val insideMargin = pattern.length - nums.sum - nums.size
+      val sideMargin = insideMargin - 1
+
+
+
+      (pattern, nums)
+    }
+
 }
--- src/test/scala/day12/solvers/ManualPreSolverSpec.scala
+++ src/test/scala/day12/solvers/ManualPreSolverSpec.scala
@@ -7,9 +7,65 @@ class ManualPreSolverSpec extends AnyFlatSpec with Matchers {
 
   behavior of ManualPresolver.getClass.getName
 
-  it should "find definite match if all should be colored" in {
-    val input =          List(("_____", List(5)))
+  it should "find definite short self overlap" in {
+    val input = List(("_____", List(3)))
+    val expectedOutput = List(("__X__", List(3)))
+
+    SpringErrorMatcher.solve(input) shouldBe expectedOutput
+  }
+
+  it should "find definite long self overlap" in {
+    val input = List(("_____", List(4)))
+    val expectedOutput = List(("_XXX_", List(4)))
+
+    SpringErrorMatcher.solve(input) shouldBe expectedOutput
+  }
+
+  it should "find definite full self overlap" in {
+    val input = List(("_____", List(5)))
     val expectedOutput = List(("XXXXX", List(5)))
+
+    SpringErrorMatcher.solve(input) shouldBe expectedOutput
+  }
+
+  it should "find definite short self overlap (multi)" in {
+    val input = List(("_________", List(3, 3)))
+    val expectedOutput = List(("__X___X__", List(3, 3)))
+
+    SpringErrorMatcher.solve(input) shouldBe expectedOutput
+  }
+
+  it should "find definite long self overlap (multi)" in {
+    val input = List(("_________", List(3, 4)))
+    val expectedOutput = List(("_XX__XXX_", List(3, 4)))
+
+    SpringErrorMatcher.solve(input) shouldBe expectedOutput
+  }
+
+  it should "find definite full self overlap (multi)" in {
+    val input = List(("_____", List(2, 2)))
+    val expectedOutput = List(("XXOXX", List(2, 2)))
+
+    SpringErrorMatcher.solve(input) shouldBe expectedOutput
+  }
+
+  it should "find definite only for parts long enough" in {
+    val input = List(("_______________", List(1, 2, 3, 4)))
+    val expectedOutput = List(("_______X___XX__", List(1, 2, 3, 4)))
+
+    SpringErrorMatcher.solve(input) shouldBe expectedOutput
+  }
+
+  it should "find definite even in complex cases" in {
+    val input = List(("_________________________", List(5, 1, 4, 2, 1, 4)))
+    val expectedOutput = List(("___XX______X_________X___", List(5, 1, 4, 2, 1, 4)))
+
+    SpringErrorMatcher.solve(input) shouldBe expectedOutput
+  }
+
+  it should "find definite full even in complex cases" in {
+    val input = List(("______________________", List(5, 1, 4, 2, 1, 4)))
+    val expectedOutput = List(("XXXXXOXOXXXXOXXOXOXXXX", List(5, 1, 4, 2, 1, 4)))
 
     SpringErrorMatcher.solve(input) shouldBe expectedOutput
   }
