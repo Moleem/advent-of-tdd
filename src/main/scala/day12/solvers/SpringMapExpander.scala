3f67058ef3e90f8562f57b8550d088b91bce0a3b
Moleem <imre.molnar.0225@gmail.com>
2023-12-12 12:32:04 +0100

    DAY-12 green

--- src/main/scala/day12/solvers/SpringMapExpander.scala
+++ src/main/scala/day12/solvers/SpringMapExpander.scala
@@ -3,5 +3,8 @@ package day12.solvers
 import utils.ProblemSolver
 
 object SpringMapExpander extends ProblemSolver[List[(String, List[Int])], List[(String, List[Int])]] {
-  override def solve(input: List[(String, List[Int])]): List[(String, List[Int])] = ???
+  override def solve(input: List[(String, List[Int])]): List[(String, List[Int])] =
+    input.map { case (pattern, nums) =>
+      (List.tabulate(5)(_ => pattern).mkString("?"), List.tabulate(5)(_ => nums).flatten)
+    }
 }
