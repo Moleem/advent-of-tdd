f64245a3d863eab32fea10fdbb8be60e6f315e0c
Moleem <imre.molnar.0225@gmail.com>
2023-12-12 22:46:11 +0100

    DAY-12 green

--- src/main/scala/day12/solvers/ManualPresolver.scala
+++ src/main/scala/day12/solvers/ManualPresolver.scala
@@ -7,12 +7,26 @@ import scala.collection.mutable
 object ManualPresolver extends ProblemSolver[List[(String, List[Int])], List[(String, List[Int])]] {
   override def solve(input: List[(String, List[Int])]): List[(String, List[Int])] =
     input.map { case (pattern, nums) =>
-      val insideMargin = pattern.length - nums.sum - nums.size
-      val sideMargin = insideMargin - 1
+      val uncertainty = pattern.length - (nums.sum + (nums.size-1))
 
+      val sb = new StringBuilder
+      (0 until uncertainty).foreach(_ => sb.append("_"))
+      nums.foreach { num =>
+        if (num > uncertainty)
+          (0 until num-uncertainty).foreach(_ => sb.append("X"))
+        else
+          (0 until (uncertainty-num)).foreach(_ => sb.deleteCharAt(sb.length-1))
 
+        (0 until uncertainty+1).foreach(_ => sb.append("_"))
+      }
 
-      (pattern, nums)
+      sb.deleteCharAt(sb.length-1)
+
+      if (sb.count(_=='X') == nums.sum) {
+        sb.indices.foreach(id => if (sb.charAt(id) == '_') sb.replace(id, id+1, "O"))
+      }
+
+      (sb.toString, nums)
     }
 
 }
