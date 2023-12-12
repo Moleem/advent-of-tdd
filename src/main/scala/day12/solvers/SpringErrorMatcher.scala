178fd0d467b3d7a2fc67ec70c91dc778cd8e0130
Moleem <imre.molnar.0225@gmail.com>
2023-12-12 12:13:11 +0100

    DAY-12 green EZ

--- src/main/scala/day12/solvers/SpringErrorMatcher.scala
+++ src/main/scala/day12/solvers/SpringErrorMatcher.scala
@@ -2,7 +2,44 @@ package day12.solvers
 
 import utils.ProblemSolver
 
+import scala.annotation.tailrec
+
 object SpringErrorMatcher extends ProblemSolver[List[(String, List[Int])], Int] {
-  override def solve(input: List[(String, List[Int])]): Int =
-    1
+
+  private def getAllPossibleValues(pattern: String) = {
+    @tailrec
+    def helper(toBeChecked: List[String], results: List[String]): List[String] = {
+      toBeChecked match {
+        case Nil => results
+        case head :: tail if !head.contains("?") =>
+          helper(tail, head :: results)
+        case head :: tail if head.contains("?") =>
+          val indexOfUnknown = head.indexOf("?")
+          val withGood = head.substring(0, indexOfUnknown) + "." + head.substring(indexOfUnknown+1)
+          val withBad = head.substring(0, indexOfUnknown) + "#" + head.substring(indexOfUnknown+1)
+          helper(List(withGood, withBad) ++ tail, results)
+      }
+    }
+
+    helper(List(pattern), List.empty)
+  }
+
+  private def getErrorClusters(s: String): List[Int] =
+    s.split("\\.").toList.filterNot(_.isEmpty).map(_.length)
+
+  /**
+   *
+   * .#.?.
+   * .#..., .#.#.
+   *
+   *
+   * */
+  override def solve(input: List[(String, List[Int])]): Int = {
+    input
+      .map { case (pattern, nums) =>
+        val possibleValues = getAllPossibleValues(pattern)
+        val errorClusters = possibleValues.map(getErrorClusters)
+        errorClusters.count(_ == nums)
+      }.sum
+  }
 }
--- src/test/scala/day12/solvers/SpringErrorMatcherSpec.scala
+++ src/test/scala/day12/solvers/SpringErrorMatcherSpec.scala
@@ -31,9 +31,63 @@ class SpringErrorMatcherSpec extends AnyFlatSpec with Matchers {
     SpringErrorMatcher.solve(input) shouldBe 1
   }
 
-  it should "return two possible arrangements if the error can go in two different places" in {
+  it should "return two possible arrangements if the error can go in two different places (1)" in {
     val input = List((".??.", List(1)))
 
     SpringErrorMatcher.solve(input) shouldBe 2
+  }
+
+  it should "return two possible arrangements if the error can go in two different places (2)" in {
+    val input = List((".?.?.", List(1)))
+
+    SpringErrorMatcher.solve(input) shouldBe 2
+  }
+
+  it should "return two possible arrangements if the error can go in two different places (3)" in {
+    val input = List((".??.??.", List(1, 2)))
+
+    SpringErrorMatcher.solve(input) shouldBe 2
+  }
+
+  it should "return four possible arrangements if the error can go in four different places (1)" in {
+    val input = List((".??.??.", List(1, 1)))
+
+    SpringErrorMatcher.solve(input) shouldBe 4
+  }
+
+  it should "work correctly with example input (1)" in {
+    val input = List(("???.###", List(1,1,3)))
+
+    SpringErrorMatcher.solve(input) shouldBe 1
+  }
+
+  it should "work correctly with example input (2)" in {
+    val input = List((".??..??...?##.", List(1,1,3)))
+
+    SpringErrorMatcher.solve(input) shouldBe 4
+  }
+
+  it should "work correctly with example input (3)" in {
+    val input = List(("?#?#?#?#?#?#?#?", List(1,3,1,6)))
+
+    SpringErrorMatcher.solve(input) shouldBe 1
+  }
+
+  it should "work correctly with example input (4)" in {
+    val input = List(("????.#...#...", List(4,1,1)))
+
+    SpringErrorMatcher.solve(input) shouldBe 1
+  }
+
+  it should "work correctly with example input (5)" in {
+    val input = List(("????.######..#####.", List(1,6,5)))
+
+    SpringErrorMatcher.solve(input) shouldBe 4
+  }
+
+  it should "work correctly with example input (6)" in {
+    val input = List(("?###????????", List(3,2,1)))
+
+    SpringErrorMatcher.solve(input) shouldBe 10
   }
 }
