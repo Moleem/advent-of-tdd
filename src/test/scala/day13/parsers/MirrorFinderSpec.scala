685a579446dea972accd4d784c32553ab3e52976
Moleem <imre.molnar.0225@gmail.com>
2023-12-13 20:15:22 +0100

    DAY-13 red

--- src/test/scala/day13/parsers/MirrorFinderSpec.scala
+++ src/test/scala/day13/parsers/MirrorFinderSpec.scala
@@ -8,15 +8,29 @@ class MirrorFinderSpec extends AnyFlatSpec with Matchers {
   behavior of MirrorFinder.getClass.getSimpleName
 
   it should "identify a vertical mirror" in {
-    val input = """#.##..##.
-                  |..#.##.#.
-                  |##......#
-                  |##......#
-                  |..#.##.#.
-                  |..##..##.
-                  |#.#.##.#.""".stripMargin
+    val input =
+      """#.##..##.
+        |..#.##.#.
+        |##......#
+        |##......#
+        |..#.##.#.
+        |..##..##.
+        |#.#.##.#.""".stripMargin
 
     MirrorFinder.parse(input) shouldBe 5
+  }
+
+  it should "identify a horizontal mirror (with a 100 multiplier)" in {
+    val input =
+      """#...##..#
+        |#....#..#
+        |..##..###
+        |#####.##.
+        |#####.##.
+        |..##..###
+        |#....#..#""".stripMargin
+
+    MirrorFinder.parse(input) shouldBe 400
   }
 
 }
