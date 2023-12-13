bb4f33666d25ef01315a4e488f7713ecf26d27db
Moleem <imre.molnar.0225@gmail.com>
2023-12-13 19:37:00 +0100

    DAY-12 make expansion parametrizable

--- src/main/scala/day12/solvers/SpringMapExpander.scala
+++ src/main/scala/day12/solvers/SpringMapExpander.scala
@@ -3,8 +3,11 @@ package day12.solvers
 import utils.ProblemSolver
 
 object SpringMapExpander extends ProblemSolver[List[(String, List[Int])], List[(String, List[Int])]] {
+
+  private val multiplier = 5
+
   override def solve(input: List[(String, List[Int])]): List[(String, List[Int])] =
     input.map { case (pattern, nums) =>
-      (List.tabulate(5)(_ => pattern).mkString("?"), List.tabulate(5)(_ => nums).flatten)
+      (List.tabulate(multiplier)(_ => pattern).mkString("?"), List.tabulate(multiplier)(_ => nums).flatten)
     }
 }
