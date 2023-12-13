4cf34992944a298278a8604df61a1e2371d7f05b
Moleem <imre.molnar.0225@gmail.com>
2023-12-13 21:54:55 +0100

    DAY-12 red

--- src/test/scala/day12/solvers/NewSolutionSpec.scala
+++ src/test/scala/day12/solvers/NewSolutionSpec.scala
@@ -4,10 +4,21 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import utils.ProblemSolver
 
+import scala.util.Try
+
 
 object NewSolution extends ProblemSolver[String, Long] {
 
-  override def solve(input: String): Long = ???
+  override def solve(input: String): Long = {
+    val Array(pattern, groupsStr) = input.split(" ", 2).map(_.toString)
+    val groups = Try(groupsStr.split(",").map(_.toInt).toList).getOrElse(List.empty[Int])
+
+    countArrangements(pattern, groups)
+  }
+
+  private def countArrangements(pattern: String, groups: List[Int], resultAccumulator: Long = 0): Long = {
+    ???
+  }
 
 }
 
@@ -16,10 +27,11 @@ class NewSolutionSpec extends AnyFlatSpec with Matchers {
 
   behavior of NewSolution.getClass.getSimpleName
 
-  it should "count a single possible arrangement if there are no errors expected, and we dont know the values" in {
-    val input = "??? "
-
-    NewSolution.solve(input) shouldBe 1
+  it should "count a single possible arrangement if there are no errors expected, and nor present" in {
+    NewSolution.solve(" ") shouldBe 1
+    NewSolution.solve("? ") shouldBe 1
+    NewSolution.solve(". ") shouldBe 1
+    NewSolution.solve(".? ") shouldBe 1
   }
 
 }
