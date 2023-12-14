cc175133c9477e48bfd78527ac09f07056974201
Moleem <imre.molnar.0225@gmail.com>
2023-12-14 15:54:04 +0100

    DAY-14 green

--- src/test/scala/day14/solvers/LoadCalculatorSpec.scala
+++ src/test/scala/day14/solvers/LoadCalculatorSpec.scala
@@ -5,7 +5,17 @@ import org.scalatest.matchers.should.Matchers
 import utils.ProblemSolver
 
 object LoadCalculator extends ProblemSolver[String, Int] {
-  override def solve(input: String): Int = ???
+  override def solve(input: String): Int = {
+    input
+      .split("\n")
+      .toList
+      .reverse
+      .zipWithIndex
+      .map { case (row, rowId) =>
+        (rowId+1)*row.count(_=='O')
+      }
+      .sum
+  }
 }
 
 class LoadCalculatorSpec extends AnyFlatSpec with Matchers {
