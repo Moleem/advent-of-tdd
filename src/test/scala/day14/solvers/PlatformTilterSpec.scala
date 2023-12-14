73004093f276175414b5a86a54acf3c906c2f670
Moleem <imre.molnar.0225@gmail.com>
2023-12-14 15:28:50 +0100

    DAY-14 green

--- src/test/scala/day14/solvers/PlatformTilterSpec.scala
+++ src/test/scala/day14/solvers/PlatformTilterSpec.scala
@@ -5,7 +5,17 @@ import org.scalatest.matchers.should.Matchers
 import utils.ProblemSolver
 
 object PlatformTilter extends ProblemSolver[String, String] {
-  override def solve(input: String): String = ???
+  override def solve(input: String): String = {
+    val originalColumns = input.split("\n").map(_.toList).toList.transpose
+    val tiltedColumns = originalColumns.map { column =>
+      column.mkString.split("#", -1).map { partBetweenFixStones =>
+        val (spaces, rollingStones) = partBetweenFixStones.partition(_ == 'O')
+        spaces.mkString + rollingStones.mkString
+      }.mkString("#")
+    }
+
+    tiltedColumns.transpose.map(_.mkString).mkString("\n")
+  }
 }
 
 class PlatformTilterSpec extends AnyFlatSpec with Matchers {
