9d49094f39921d7526d74bec35148f09d1f787f6
Moleem <imre.molnar.0225@gmail.com>
2023-12-15 22:18:33 +0100

    DAY-15 green

--- src/test/scala/day15/solvers/BoxSumamrizerSpec.scala
+++ src/test/scala/day15/solvers/BoxSumamrizerSpec.scala
@@ -6,7 +6,14 @@ import org.scalatest.matchers.should.Matchers
 import utils.ProblemSolver
 
 object BoxSummarizer extends ProblemSolver[Map[Int, List[Lens]], Int] {
-  override def solve(input: Map[Int, List[Lens]]): Int = ???
+
+  override def solve(input: Map[Int, List[Lens]]): Int =
+    input.flatMap { case (boxId, lenses) =>
+      lenses.zipWithIndex.map { case (lens, lensId) =>
+        (boxId+1) * (lensId+1) * lens.focalLength
+      }
+    }.sum
+
 }
 
 class BoxSumamrizerSpec extends AnyFlatSpec with Matchers {
