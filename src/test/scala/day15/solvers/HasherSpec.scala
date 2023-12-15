84eb2e8570ce84015b972ce006ebd84bf01bc330
Moleem <imre.molnar.0225@gmail.com>
2023-12-15 19:06:23 +0100

    DAY-15 green

--- src/test/scala/day15/solvers/HasherSpec.scala
+++ src/test/scala/day15/solvers/HasherSpec.scala
@@ -4,8 +4,22 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import utils.ProblemSolver
 
+import scala.annotation.tailrec
+
 object Hasher extends ProblemSolver[String, Int] {
-  override def solve(input: String): Int = ???
+  override def solve(input: String): Int =
+    input.split(",").map(_.toArray.toList).map(calculateHash).sum
+
+  private def calculateHash(input: List[Char]): Int = {
+    @tailrec
+    def helper(input: List[Char], result: Int): Int =
+      input match {
+        case Nil => result
+        case head::tail => helper(tail, ((head.toInt + result) * 17 ) % 256)
+      }
+
+    helper(input, 0)
+  }
 }
 
 class HasherSpec extends AnyFlatSpec with Matchers {
