49826d24279bd2ef1d3e27dbf920501d2d1cc292
Moleem <imre.molnar.0225@gmail.com>
2023-12-15 20:24:12 +0100

    DAY-15 refactor

--- src/test/scala/day15/solvers/BoxerSpec.scala
+++ src/test/scala/day15/solvers/BoxerSpec.scala
@@ -8,13 +8,15 @@ import scala.collection.immutable.Queue
 
 case class Lens(label: String, focalLength: Int)
 
-object Boxer extends ProblemSolver[String, List[Queue[Lens]]] {
-  override def solve(input: String): List[Queue[Lens]] = {
+object Boxer extends ProblemSolver[String, Map[Int, Queue[Lens]]] {
+  override def solve(input: String): Map[Int, Queue[Lens]] = {
     val inputParts = input.split("=", -1)
     val label = inputParts(0)
     val focalLength = inputParts(1).toInt
 
-    List(Queue(Lens(label, focalLength)))
+    Map(
+      0 -> Queue(Lens(label, focalLength))
+    )
   }
 }
 
@@ -22,9 +24,11 @@ class BoxerSpec extends AnyFlatSpec with Matchers {
 
   behavior of Boxer.getClass.getSimpleName
 
-  it should "correctly insert a lens" in {
+  it should "correctly insert a lens to a new bucket" in {
     val input = "rn=1"
-    val expectedResult = List(Queue(Lens("rn", 1)))
+    val expectedResult = Map(
+      0 -> Queue(Lens("rn", 1))
+    )
     Boxer.solve(input) shouldBe expectedResult
   }
 
