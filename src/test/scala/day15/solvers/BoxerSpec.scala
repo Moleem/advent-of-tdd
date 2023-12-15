a91a0b55df83f140c6781853ec9049e98dd911ce
Moleem <imre.molnar.0225@gmail.com>
2023-12-15 22:02:44 +0100

    DAY-15 green

--- src/test/scala/day15/solvers/BoxerSpec.scala
+++ src/test/scala/day15/solvers/BoxerSpec.scala
@@ -9,17 +9,26 @@ case class Lens(label: String, focalLength: Int)
 object Boxer extends ProblemSolver[String, Map[Int, List[Lens]]] {
   override def solve(input: String): Map[Int, List[Lens]] = {
     input.split(",").foldLeft(Map.empty[Int, List[Lens]]) { case (boxes, operation) =>
-      val parts = operation.split("=", -1)
-      val label = parts(0)
-      val focalLength = parts(1).toInt
-      val index = Hasher.solve(label)
-      (index, List(Lens(label, focalLength)))
+      if (operation.contains('=')) {
+        val parts = operation.split("=", -1)
+        val label = parts(0)
+        val focalLength = parts(1).toInt
+        val index = Hasher.solve(label)
 
-      boxes.updatedWith(index) {
-        case None => Some(List(Lens(label, focalLength)))
-        case Some(existingBoxContent) => Some(existingBoxContent :+ Lens(label, focalLength))
+        boxes.updatedWith(index) {
+          case None => Some(List(Lens(label, focalLength)))
+          case Some(existingBoxContent) => Some(existingBoxContent :+ Lens(label, focalLength))
+        }
+      } else {
+        val label = operation.stripSuffix("-")
+        val index = Hasher.solve(label)
+
+        boxes.updatedWith(index) {
+            case None => None
+            case Some(existingBoxContent) => Some(existingBoxContent.filterNot(_.label == label))
+        }
       }
-    }
+    }.filterNot(_._2.isEmpty)
   }
 }
 
