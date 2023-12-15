adaf4cb205ce4b94d392cb885fc34f97c0ab63d1
Moleem <imre.molnar.0225@gmail.com>
2023-12-15 21:56:31 +0100

    DAY-15 green

--- src/test/scala/day15/solvers/BoxerSpec.scala
+++ src/test/scala/day15/solvers/BoxerSpec.scala
@@ -4,19 +4,22 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import utils.ProblemSolver
 
-import scala.collection.immutable.Queue
-
 case class Lens(label: String, focalLength: Int)
 
-object Boxer extends ProblemSolver[String, Map[Int, Queue[Lens]]] {
-  override def solve(input: String): Map[Int, Queue[Lens]] = {
-    input.split(",").map { operation =>
+object Boxer extends ProblemSolver[String, Map[Int, List[Lens]]] {
+  override def solve(input: String): Map[Int, List[Lens]] = {
+    input.split(",").foldLeft(Map.empty[Int, List[Lens]]) { case (boxes, operation) =>
       val parts = operation.split("=", -1)
       val label = parts(0)
       val focalLength = parts(1).toInt
       val index = Hasher.solve(label)
-      (index, Queue(Lens(label, focalLength)))
-    }.toMap
+      (index, List(Lens(label, focalLength)))
+
+      boxes.updatedWith(index) {
+        case None => Some(List(Lens(label, focalLength)))
+        case Some(existingBoxContent) => Some(existingBoxContent :+ Lens(label, focalLength))
+      }
+    }
   }
 }
 
@@ -27,7 +30,7 @@ class BoxerSpec extends AnyFlatSpec with Matchers {
   it should "correctly insert a lens to a new bucket" in {
     val input = "rn=1"
     val expectedResult = Map(
-      0 -> Queue(Lens("rn", 1))
+      0 -> List(Lens("rn", 1))
     )
     Boxer.solve(input) shouldBe expectedResult
   }
@@ -35,8 +38,8 @@ class BoxerSpec extends AnyFlatSpec with Matchers {
   it should "correctly insert a second lens to a different bucket" in {
     val input = "rn=1,qp=3"
     val expectedResult = Map(
-      0 -> Queue(Lens("rn", 1)),
-      1 -> Queue(Lens("qp", 3))
+      0 -> List(Lens("rn", 1)),
+      1 -> List(Lens("qp", 3))
     )
     Boxer.solve(input) shouldBe expectedResult
   }
@@ -44,7 +47,7 @@ class BoxerSpec extends AnyFlatSpec with Matchers {
   it should "correctly insert a second lens to a further bucket" in {
     val input = "pc=4"
     val expectedResult = Map(
-      3 -> Queue(Lens("pc", 4))
+      3 -> List(Lens("pc", 4))
     )
     Boxer.solve(input) shouldBe expectedResult
   }
@@ -52,7 +55,7 @@ class BoxerSpec extends AnyFlatSpec with Matchers {
   it should "correctly insert a lens into an existing bucket" in {
     val input = "rn=1,cm=2"
     val expectedResult = Map(
-      0 -> Queue(Lens("rn", 1), Lens("cm", 2))
+      0 -> List(Lens("rn", 1), Lens("cm", 2))
     )
     Boxer.solve(input) shouldBe expectedResult
   }
