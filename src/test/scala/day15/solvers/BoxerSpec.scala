ff4a6d7acaba7e5ddfc44ab4f304774f9631f861
Moleem <imre.molnar.0225@gmail.com>
2023-12-15 20:14:13 +0100

    DAY-15 green

--- src/test/scala/day15/solvers/BoxerSpec.scala
+++ src/test/scala/day15/solvers/BoxerSpec.scala
@@ -9,7 +9,13 @@ import scala.collection.immutable.Queue
 case class Lens(label: String, focalLength: Int)
 
 object Boxer extends ProblemSolver[String, List[Queue[Lens]]] {
-  override def solve(input: String): List[Queue[Lens]] = ???
+  override def solve(input: String): List[Queue[Lens]] = {
+    val inputParts = input.split("=", -1)
+    val label = inputParts(0)
+    val focalLength = inputParts(1).toInt
+
+    List(Queue(Lens(label, focalLength)))
+  }
 }
 
 class BoxerSpec extends AnyFlatSpec with Matchers {
