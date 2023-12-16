c9f30255aab012e4aea005e216399fdbf2f5aa0d
Moleem <imre.molnar.0225@gmail.com>
2023-12-16 14:04:53 +0100

    DAY-16 green

--- src/test/scala/day16/solvers/EnergizedTileFinderSpec.scala
+++ src/test/scala/day16/solvers/EnergizedTileFinderSpec.scala
@@ -48,8 +48,8 @@ case object VerticalSplitter extends Tile {
   override def handleLight(coordinates: (Int, Int), fromDirection: Direction): List[((Int, Int), Direction)] =
     fromDirection match {
       case Right | Left => List(
-        ((coordinates._1, coordinates._2+1), Left),
-        ((coordinates._1, coordinates._2-1), Right)
+        ((coordinates._1+1, coordinates._2), Up),
+        ((coordinates._1-1, coordinates._2), Down)
       )
       case Up => List(
         ((coordinates._1 - 1, coordinates._2), Up)
@@ -64,16 +64,16 @@ case object DownLeaningMirror extends Tile {
   override def handleLight(coordinates: (Int, Int), fromDirection: Direction): List[((Int, Int), Direction)] =
     fromDirection match {
       case Right => List(
-        ((coordinates._1+1, coordinates._2), Up)
+        ((coordinates._1+1, coordinates._2), Down)
       )
       case Left => List(
-        ((coordinates._1-1, coordinates._2), Down)
+        ((coordinates._1-1, coordinates._2), Up)
       )
       case Up => List(
-        ((coordinates._1, coordinates._2-1), Right)
+        ((coordinates._1, coordinates._2-1), Left)
       )
       case Down => List(
-        ((coordinates._1, coordinates._2+1), Left)
+        ((coordinates._1, coordinates._2+1), Right)
       )
     }
 }
