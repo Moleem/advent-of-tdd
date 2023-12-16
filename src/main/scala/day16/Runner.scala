6a27ec0e08197eb46899f5242f7fb54805d60a6e
Moleem <imre.molnar.0225@gmail.com>
2023-12-16 15:49:04 +0100

    DAY-16 task2 solved... Was it easy to write? Yes! Was it slow to run? Yes! Can it be improved significantly with caching? Yes! Will I do it? No!

--- src/main/scala/day16/Runner.scala
+++ src/main/scala/day16/Runner.scala
@@ -3,7 +3,7 @@ package day16
 import day16.solvers.EnergizedTileFinder
 import utils.{PrintSolution, ProblemSolver}
 import utils.parsers.StringReader
-import day16.model.Right
+import day16.model.{Down, Right, Up, Left}
 
 object Runner extends App {
 
@@ -17,10 +17,27 @@ object Runner extends App {
       ((input: String) => input.count(_ == '#'))
   )
 
-//  PrintSolution(
-//    inputFileName = s"/$dayNum/input-2.txt",
-//    contentParser = DummyTask2Parser,
-//    problemSolver = DummyTask2Solver
-//  )
+  PrintSolution(
+    inputFileName = s"/$dayNum/input-2.txt",
+    contentParser = StringReader,
+    problemSolver = (input: String) => {
+      val matrix = input.split("\n").map(_.toList).toList
+      val rowCount = matrix.size
+      val colCount = matrix.head.size
+
+      val possibleStartsFromTop = (0 until colCount).map(c => ((0, c), Down))
+      val possibleStartsFromBottom = (0 until colCount).map(c => ((rowCount-1, c), Up))
+      val possibleStartsFromLeft = (0 until rowCount).map(r => ((r, 0), Right))
+      val possibleStartsFromRight = (0 until rowCount).map(r => ((r, colCount-1), Left))
+
+      val possibleStarts = possibleStartsFromTop ++ possibleStartsFromBottom ++ possibleStartsFromLeft ++ possibleStartsFromRight
+      val totalStartCount = possibleStarts.size
+
+      possibleStarts.zipWithIndex.map { case (((row, col), direction), id) =>
+        println(s"$id / $totalStartCount")
+        new EnergizedTileFinder(row, col, direction).solve(input).count(_=='#')
+      }.max
+    }
+  )
 
 }
