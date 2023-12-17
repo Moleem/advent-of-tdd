ab2d0af9ef06c8c2073093e589df6e62a2b62c4d
Moleem <imre.molnar.0225@gmail.com>
2023-12-17 19:13:30 +0100

    DAY-17 green, after a wasted sunday

--- src/test/scala/day17/solvers/MinimizeHeatLossSpec.scala
+++ src/test/scala/day17/solvers/MinimizeHeatLossSpec.scala
@@ -9,21 +9,73 @@ class MinimizeHeatLossSpec extends AnyFlatSpec with Matchers {
   behavior of "MinimizeHeatLoss"
 
   it should "find the path with the least heat loss" in {
-    val input = """2413432311323
-                  |3215453535623
-                  |3255245654254
-                  |3446585845452
-                  |4546657867536
-                  |1438598798454
-                  |4457876987766
-                  |3637877979653
-                  |4654967986887
-                  |4564679986453
-                  |1224686865563
-                  |2546548887735
-                  |4322674655533""".stripMargin
+    val input =
+      """2413432311323
+        |3215453535623
+        |3255245654254
+        |3446585845452
+        |4546657867536
+        |1438598798454
+        |4457876987766
+        |3637877979653
+        |4654967986887
+        |4564679986453
+        |1224686865563
+        |2546548887735
+        |4322674655533""".stripMargin
 
     MinimizeHeatLoss.solve(input) shouldBe 102
+  }
+
+  it should "find the path with the least heat loss in a small example (1)" in {
+    val input =
+      """11111
+        |11111
+        |11111
+        |11111
+        |11111""".stripMargin
+
+    MinimizeHeatLoss.solve(input) shouldBe 8
+  }
+
+  it should "find the path with the least heat loss in a small example (2)" in {
+    val input =
+      """11111
+        |11111
+        |11111
+        |11111
+        |11112""".stripMargin
+
+    MinimizeHeatLoss.solve(input) shouldBe 9
+  }
+
+  it should "find the path with the least heat loss in a small example (3)" in {
+    val input =
+      """21111
+        |11111
+        |11111
+        |11111
+        |11111""".stripMargin
+
+    MinimizeHeatLoss.solve(input) shouldBe 8
+  }
+
+  it should "find the path with the least heat loss in a small example (4)" in {
+    val input =
+      """21111
+        |11121
+        |11111
+        |12111
+        |11111""".stripMargin
+
+    MinimizeHeatLoss.solve(input) shouldBe 8
+  }
+  it should "find the path with the least heat loss in a small example (5)" in {
+    val input =
+      """12222
+        |11111""".stripMargin
+
+    MinimizeHeatLoss.solve(input) shouldBe 6
   }
 
 }
