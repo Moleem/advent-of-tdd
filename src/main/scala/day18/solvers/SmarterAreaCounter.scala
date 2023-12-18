52b391373ff634be13c79c407463b5250dbccb02
Moleem <imre.molnar.0225@gmail.com>
2023-12-18 21:53:44 +0100

    DAY-18 green

--- src/main/scala/day18/solvers/SmarterAreaCounter.scala
+++ src/main/scala/day18/solvers/SmarterAreaCounter.scala
@@ -3,5 +3,15 @@ package day18.solvers
 import utils.ProblemSolver
 
 object SmarterAreaCounter extends ProblemSolver[List[(Char, Int)], Long] {
-  override def solve(input: List[(Char, Int)]): Long = ???
+  override def solve(input: List[(Char, Int)]): Long = {
+    var area = 0L;
+
+    input.foreach { case (direction, distance) =>
+      direction match {
+        case 'R' => area += distance
+      }
+    }
+
+    area
+  }
 }
--- src/test/scala/day18/solvers/SmarterAreaCounterSpec.scala
+++ src/test/scala/day18/solvers/SmarterAreaCounterSpec.scala
@@ -7,7 +7,7 @@ class SmarterAreaCounterSpec extends AnyFlatSpec with Matchers {
 
   behavior of SmarterAreaCounter.getClass.getSimpleName
 
-  it should "be able to count area R only" in {
+  it should "be able to count area for a line" in {
     val input = List(('R', 3))
     // ###
     SmarterAreaCounter.solve(input) shouldBe 3
