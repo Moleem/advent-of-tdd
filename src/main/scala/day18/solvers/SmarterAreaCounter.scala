a472c617e7e1e989e262d1708db064e49fb4ac8d
Moleem <imre.molnar.0225@gmail.com>
2023-12-19 16:47:55 +0100

    DAY-18 refactored, looks a bit better, works a bit better

--- src/main/scala/day18/solvers/SmarterAreaCounter.scala
+++ src/main/scala/day18/solvers/SmarterAreaCounter.scala
@@ -75,118 +75,71 @@ object SmarterAreaCounter extends ProblemSolver[List[(Char, Int)], Long] {
       intervals.addOne(intervalsForRow)
     }
 
-    // add first row intervals
-    val firstRow = intervals.head
-    val nextRow = intervals.tail.head
-    val firstRowCorners = new ListBuffer[CornerPoint]
-    firstRowCorners.addAll(corners.filter(_.row == 0).toList.sortBy(_.col))
-    while (firstRowCorners.nonEmpty) {
-      val firstCorner = firstRowCorners.head
-      val secondCorner = firstRowCorners.tail.head
-      (firstCorner.cornerType, secondCorner.cornerType) match {
-        case (┌, ┐) =>
-          firstRow.addOne((firstCorner.col, secondCorner.col))
-          nextRow.addOne((firstCorner.col, secondCorner.col))
-      }
-      firstRowCorners.remove(0, 2)
-    }
-
-    // add last row intervals
-    val lastRow = intervals.last
-    val lastRowCorners = new ListBuffer[CornerPoint]
-    lastRowCorners.addAll(corners.filter(_.row == corners.map(_.row).max).toList.sortBy(_.col))
-
-    while (lastRowCorners.nonEmpty) {
-      val firstCorner = lastRowCorners.head
-      val secondCorner = lastRowCorners.tail.head
-      (firstCorner.cornerType, secondCorner.cornerType) match {
-        case (└, ┘) =>
-          lastRow.addOne((firstCorner.col, secondCorner.col))
-      }
-      lastRowCorners.remove(0, 2)
-    }
-
-    // middle rows
-    (corners.map(_.row).min + 1 until corners.map(_.row).max).foreach { row =>
-      val cornersInRow = new ListBuffer[CornerPoint]
-      cornersInRow.addAll(corners.filter(_.row == row).toList.sortBy(_.col))
+    (corners.map(_.row).min to corners.map(_.row).max).foreach { row =>
+      val cornersOfThisRow = corners.filter(_.row == row).toList.sortBy(_.col)
 
       val thisRow = intervals(row)
-      val nextRow = intervals(row+1)
 
-      if (cornersInRow.isEmpty) {
-        nextRow.addAll(thisRow)
-      } else {
-        while (cornersInRow.nonEmpty) {
-          val firstCorner = cornersInRow.head
-          val secondCorner = cornersInRow.tail.head
-          (firstCorner.cornerType, secondCorner.cornerType) match {
-            case (┌, ┐) =>
-              thisRow.addOne((firstCorner.col, secondCorner.col))
-              nextRow.addOne((firstCorner.col, secondCorner.col))
-
-            case (└, ┘) =>
-              thisRow.addOne((firstCorner.col, secondCorner.col))
-
-            case (└, ┐) =>
-              if (nextRow.isEmpty)
-                nextRow.addAll(thisRow)
-
-              val intervalToBeReduced = nextRow.find(_._1 == firstCorner.col)
-              val reducedInterval = intervalToBeReduced.map(_.copy(_1 = secondCorner.col))
-              val intervalToBeExpanded = thisRow.find(_._2 == firstCorner.col)
-              val expandedInterval = intervalToBeExpanded.map(_.copy(_2 = secondCorner.col))
-
-              intervalToBeExpanded.foreach(thisRow.remove)
-              expandedInterval.foreach(thisRow.addOne)
-
-              intervalToBeReduced.foreach(nextRow.remove)
-              reducedInterval.foreach(nextRow.addOne)
-
-            case (┌, ┘) =>
-              if (nextRow.isEmpty)
-                nextRow.addAll(thisRow)
-
-              val intervalToBeReduced = nextRow.find(_._2 == secondCorner.col)
-              val reducedInterval = intervalToBeReduced.map(_.copy(_2 = firstCorner.col))
-              val intervalToBeExpanded = thisRow.find(_._1 == secondCorner.col)
-              val expandedInterval = intervalToBeExpanded.map(_.copy(_1 = firstCorner.col))
-
-              intervalToBeExpanded.foreach(thisRow.remove)
-              expandedInterval.foreach(thisRow.addOne)
-
-
-              intervalToBeReduced.foreach(nextRow.remove)
-              reducedInterval.foreach(nextRow.addOne)
-          }
-
-          cornersInRow.remove(0, 2)
+      cornersOfThisRow.sliding(2).foreach { case List(c1, c2) =>
+        (c1.cornerType, c2.cornerType) match {
+          case (┌, ┐) =>
+            thisRow.addOne((c1.col, c2.col))
+          case (└, ┐) =>
+            thisRow.find(_._2 == c1.col).foreach { intervalToBeExpanded =>
+              thisRow.remove(intervalToBeExpanded)
+              thisRow.addOne(intervalToBeExpanded.copy(_2 = c2.col))
+            }
+          case (┌, ┘) =>
+            thisRow.find(_._1 == c2.col).foreach { intervalToBeExpanded =>
+              thisRow.remove(intervalToBeExpanded)
+              thisRow.addOne(intervalToBeExpanded.copy(_1 = c1.col))
+            }
+          case _ => // do nothing
         }
       }
 
-      // ┌ ┐
-      //  - if there are no previous intervals overlapping,
-      //    then starts a new interval
-      //  - if it falls into an existing interval,
-      //    then it splits the interval in the next round
+      if (row != corners.map(_.row).max) {
+        val nextRow = intervals(row+1)
+        nextRow.addAll(thisRow)
 
-      // └ ┐
-      //  - if the first corner aligns with an interval start,
-      //    then that interval will start from the second corner in the next round
-      //  - if the first corner aligns with an interval end,
-      //    then that interval will end from the second corner in the next round
+        cornersOfThisRow.sliding(2).foreach { case List(c1, c2) =>
+          (c1.cornerType, c2.cornerType) match {
+            case (└, ┘) =>
+              nextRow.remove((c1.col, c2.col))
+            case (└, ┐) =>
+              nextRow.find(_._1 == c1.col).foreach { intervalToBeReduced =>
+                nextRow.remove(intervalToBeReduced)
+                nextRow.addOne(intervalToBeReduced.copy(_1 = c2.col))
+              }
+            case (┌, ┘) =>
+              nextRow.find(_._2 == c2.col).foreach { intervalToBeReduced =>
+                nextRow.remove(intervalToBeReduced)
+                nextRow.addOne(intervalToBeReduced.copy(_2 = c1.col))
+              }
+            case _ => // do nothing
+          }
+        }
+      }
 
-      // ┌ ┘
-      //  - if the second corner aligns with an interval start,
-      //    then that interval will start from the first corner in the next round
-      //  - if the second corner aligns with an interval end,
-      //    then that interval will end from the first corner in the next round
 
-      // └ ┘
-      //  - if matches an existing interval,
-      //    then that interval will not appear in the next round
-      //  - if bridges two existing intervals,
-      //    then those intervals will be merged in the next round
+
+      //|-----|------------------|-------------------|
+      //|     | modifies current | modifies next     |
+      //|-----|------------------|-------------------|
+      //|┌  ┐ | add              | add same          |
+      //|-----|------------------|-------------------|
+      //|     | none             | add same          |
+      //|-----|------------------|-------------------|
+      //|└  ┘ | none             | remove            |
+      //|-----|------------------|-------------------|
+      //|└  ┐ | none             | (r) decrease      |
+      //|-----|------------------|-------------------|
+      //|└  ┐ | (e) increase     | add same          |
+      //|-----|------------------|-------------------|
+      //|┌  ┘ | none             | (r) decrease      |
+      //|-----|------------------|-------------------|
+      //|┌  ┘ | (e) increase     | add same          |
+      //|-----|------------------|-------------------|
     }
 
     val result = intervals.flatMap(intervalsForRow => intervalsForRow.map{ case (start, end) => 1+end-start}).sum
