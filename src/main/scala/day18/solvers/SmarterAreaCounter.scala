02eb2be1fa927ad67fd8bf4c861e755699cb12df
Moleem <imre.molnar.0225@gmail.com>
2023-12-19 09:39:31 +0100

    DAY-18 new approach new tests

--- src/main/scala/day18/solvers/SmarterAreaCounter.scala
+++ src/main/scala/day18/solvers/SmarterAreaCounter.scala
@@ -3,18 +3,5 @@ package day18.solvers
 import utils.ProblemSolver
 
 object SmarterAreaCounter extends ProblemSolver[List[(Char, Int)], Long] {
-  override def solve(input: List[(Char, Int)]): Long = {
-    var area = 0L;
-
-    input.foreach { case (direction, distance) =>
-      direction match {
-        case 'R' => area += (distance+1)
-        case 'D' => area *= (distance+1)
-        case 'L' =>
-        case 'U' =>
-      }
-    }
-
-    area
-  }
+  override def solve(input: List[(Char, Int)]): Long = ???
 }
